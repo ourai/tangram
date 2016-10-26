fd038988d2ac000ef020bde2e9b40ac5cbd4c2bd
Ourai Lin <ourairyu@gmail.com>
2016-10-26 19:23:11 +0800

    Build with Gulp instead of Grunt

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const gulp = require("gulp");
+const concat = require("gulp-concat");
+const sass = require("gulp-sass");
+const cssmin = require("gulp-cssmin");
+const rename = require("gulp-rename");
+
+gulp.task("concat", function() {
+  gulp
+    .src([
+      "./src/utils/_box-model.scss",
+      "./src/components/_boxes.scss",
+      "./src/layouts/_sandwich.scss",
+      "./src/layouts/_frameset.scss",
+      "./src/layouts/_scroll.scss"
+    ])
+    .pipe(concat("_tangram.scss"))
+    .pipe(gulp.dest("./"));
+});
+
+gulp.task("compile", function() {
+  gulp
+    .src("src/**/*.scss")
+    .pipe(sass({outputStyle: "expanded", noLineComments: true}).on("error", sass.logError))
+    .pipe(gulp.dest("./"));
+});
+
+gulp.task("minify", function() {
+  gulp
+    .src("./tangram.css")
+    .pipe(cssmin({keepSpecialComments: 0}))
+    .pipe(rename({suffix: ".min"}))
+    .pipe(gulp.dest("./"))
+});
+
+gulp.task("default", ["concat", "compile", "minify"]);
