ae6c6c7a47b611ca140a9fa5f07d72d9cf4546ff
Ourai Lin <ourairyu@gmail.com>
2016-10-26 23:48:21 +0800

    Add gulp task for test

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const concat = require("gulp-concat");
 const sass = require("gulp-sass");
 const cssmin = require("gulp-cssmin");
 const rename = require("gulp-rename");
+const copy = require("gulp-copy");
 
 gulp.task("concat", function() {
   gulp
@@ -29,6 +30,17 @@ gulp.task("compile", function() {
     .pipe(cssmin({keepSpecialComments: 0}))
     .pipe(rename({suffix: ".min"}))
     .pipe(gulp.dest("./"));
+});
+
+gulp.task("test", function() {
+  gulp
+    .src("./_tangram.scss")
+    .pipe(copy("./test/stylesheets"));
+
+  gulp
+    .src("test/**/*.scss")
+    .pipe(sass({outputStyle: "expanded", noLineComments: true}).on("error", sass.logError))
+    .pipe(gulp.dest("./test"))
 });
 
 gulp.task("default", ["concat", "compile"]);
